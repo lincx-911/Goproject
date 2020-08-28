a815906507fa4e728868b6cd434aae3a159c28a3
lincx_py <43648278+lincxpy@users.noreply.github.com>
2020-08-28 20:26:20 +0800

    Update code.go

--- forum/common/code.go
+++ forum/common/code.go
@@ -15,9 +15,9 @@ const (
 const (
 	HEADER_TOKEN        = "token"
 	HEADER_VERSION      = "version"
-	SECRET_KEY          = "54f12kkkc8-1fff56-45chg1-a3fsjc-2c546bc2b"
-	JWT_ISSUE           = "LINCX_JAVA"
-	VERSION_ID = "lincx_v001"
+	SECRET_KEY          = "54f12kkkc8-1fxxxxxxxxxxxxxxxxxx"
+	JWT_ISSUE           = "LINCXxxxxxx"
+	VERSION_ID = "lincxxxxxxxxx1"
 	TokenExpireDuration = time.Hour*24
 	PAGE_SIZE = 10
 	
